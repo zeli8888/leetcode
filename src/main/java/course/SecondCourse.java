a714ee8194567f15d0875a003a77fabdf90fad1d
Edwin <lize201@mails.ucas.ac.cn>
2024-10-21 00:08:38 +0100

    secondclass

---
+++ src/main/java/course/SecondCourse.java
@@ -0,0 +1,16 @@
+package course;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @Author : Ze Li
+ * @Date : 20/10/2024 14:59
+ * @Version : V1.0
+ * @Description :
+ */
+public class SecondCourse {
+    public static void main(String[] args) {
+        Queue<Integer> integers = new LinkedList<>();
+    }
+}
