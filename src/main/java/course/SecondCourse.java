9f6bf39b905e017d03d32661d6242fddbe5f1639
Edwin <lize201@mails.ucas.ac.cn>
2024-10-21 17:45:14 +0100

    doneclass2

--- src/main/java/course/SecondCourse.java
+++ src/main/java/course/SecondCourse.java
@@ -1,5 +1,6 @@
 package course;
 
+import javax.swing.tree.TreeNode;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -10,7 +11,69 @@ import java.util.Queue;
  * @Description :
  */
 public class SecondCourse {
+    private SecondCourse left;
+    private SecondCourse right;
+    private String val;
     public static void main(String[] args) {
-        Queue<Integer> integers = new LinkedList<>();
+        SecondCourse root = new SecondCourse("A");
+        root.left = new SecondCourse("B");
+        root.right = new SecondCourse("C");
+        root.left.left = new SecondCourse("D");
+        root.left.right = new SecondCourse("E");
+        root.right.left = new SecondCourse("F");
+        root.right.right = new SecondCourse("G");
+        System.out.println("Pre-order traversal of binary tree is: ");
+        preOrderTraversal(root);
+        System.out.println();
+        System.out.println("In-order traversal of binary tree is: ");
+        inOrderTraversal(root);
+        System.out.println();
+        System.out.println("Post-order traversal of binary tree is: ");
+        postOrderTraversal(root);
     }
+
+    public static void preOrderTraversal(SecondCourse root) {
+        System.out.print(root.val);
+        if (root.left != null){
+            preOrderTraversal(root.left);
+        }
+        if (root.right != null){
+            preOrderTraversal(root.right);
+        }
+    }
+
+    public static void inOrderTraversal(SecondCourse root) {
+        if (root.left != null){
+            inOrderTraversal(root.left);
+        }
+        System.out.print(root.val);
+        if (root.right != null){
+            inOrderTraversal(root.right);
+        }
+    }
+
+    public static void postOrderTraversal(SecondCourse root) {
+        if (root.left != null){
+            postOrderTraversal(root.left);
+        }
+        if (root.right != null){
+            postOrderTraversal(root.right);
+        }
+        System.out.print(root.val);
+    }
+
+    public SecondCourse(SecondCourse left, SecondCourse right, String val) {
+        this.left = left;
+        this.right = right;
+        this.val = val;
+    }
+
+    public SecondCourse(String val) {
+        this.val = val;
+    }
+
+    public SecondCourse() {
+    }
+
+
 }
