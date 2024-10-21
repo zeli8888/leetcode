e8bb1da656e76123305f94051619ad31b4c5fb2b
liz <65708133+wlqqlz@users.noreply.github.com>
2024-10-21 14:06:40 +0100

    morecomplex

--- src/main/java/exercise/ImplementTriePrefixTree208.java
+++ src/main/java/exercise/ImplementTriePrefixTree208.java
@@ -2,6 +2,8 @@ package exercise;
 
 import java.sql.Array;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @Author : Ze Li
@@ -10,19 +12,42 @@ import java.util.ArrayList;
  * @Description :
  */
 public class ImplementTriePrefixTree208 {
+    int count;
+    boolean isWord;
+    ImplementTriePrefixTree208[] node = new ImplementTriePrefixTree208[26];
     public ImplementTriePrefixTree208() {
-        ArrayList<String> words = new Set<String>();
+        count = 0;
     }
 
     public void insert(String word) {
+        ImplementTriePrefixTree208 cur = this;
+        for(char idx: word.toCharArray()) {
+            idx -= 'a';
+            if(cur.node[idx] == null)
+                cur.node[idx] = new ImplementTriePrefixTree208();
+            ++cur.count;
+            cur = cur.node[idx];
+        }
+        ++cur.count;
+        cur.isWord = true;
+    }
 
+    private boolean searchWordPrefix(String s, boolean isPrefix) {
+        ImplementTriePrefixTree208 cur = this;
+        for(char idx: s.toCharArray()) {
+            idx -= 'a';
+            if(cur.node[idx] == null)
+                return false;
+            cur = cur.node[idx];
+        }
+        return isPrefix ? cur.count > 0 : cur.isWord;
     }
 
     public boolean search(String word) {
-
+        return searchWordPrefix(word, false);
     }
 
     public boolean startsWith(String prefix) {
-
+        return searchWordPrefix(prefix, true);
     }
 }
