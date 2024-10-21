b4e9a5aa3900dd05f516ff65b311d96a3fdc2d56
liz <65708133+wlqqlz@users.noreply.github.com>
2024-10-21 13:49:14 +0100

    moretodo

--- src/main/java/exercise/ImplementTriePrefixTree208.java
+++ src/main/java/exercise/ImplementTriePrefixTree208.java
@@ -1,5 +1,8 @@
 package exercise;
 
+import java.sql.Array;
+import java.util.ArrayList;
+
 /**
  * @Author : Ze Li
  * @Date : 21/10/2024 13:26
@@ -7,4 +10,19 @@ package exercise;
  * @Description :
  */
 public class ImplementTriePrefixTree208 {
+    public ImplementTriePrefixTree208() {
+        ArrayList<String> words = new Set<String>();
+    }
+
+    public void insert(String word) {
+
+    }
+
+    public boolean search(String word) {
+
+    }
+
+    public boolean startsWith(String prefix) {
+
+    }
 }
