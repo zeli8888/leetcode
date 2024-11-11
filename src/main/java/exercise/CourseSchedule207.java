51882a81bed1dd39887f7ce0db381d5be5461928
liz <65708133+wlqqlz@users.noreply.github.com>
2024-11-11 15:52:55 +0000

    done

--- src/main/java/exercise/CourseSchedule207.java
+++ src/main/java/exercise/CourseSchedule207.java
@@ -1,5 +1,8 @@
 package exercise;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.Stack;
 
 /**
@@ -11,20 +14,17 @@ import java.util.Stack;
 public class CourseSchedule207 {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         class TreeNode {
-//            many parents, many children
-            TreeNode parent;
-            TreeNode child;
-
-            public TreeNode() {
-            }
+            List<TreeNode> parent;
+            List<TreeNode> child;
+            public TreeNode() {}
         }
         TreeNode[] nodes = new TreeNode[numCourses];
         for (int i = 0; i < numCourses; i++) {
             nodes[i] = new TreeNode();
         }
         for (int[] prerequisite : prerequisites) {
-            nodes[prerequisite[0]].parent = nodes[prerequisite[1]];
-            nodes[prerequisite[1]].child = nodes[prerequisite[0]];
+            nodes[prerequisite[0]].parent.add(nodes[prerequisite[1]]);
+            nodes[prerequisite[1]].child.add(nodes[prerequisite[0]]);
         }
         Stack<TreeNode> rootNodes = null;
         for (int i = 0; i < numCourses; i++) {
@@ -33,12 +33,28 @@ public class CourseSchedule207 {
                 rootNodes.push(nodes[i]);
             }
         }
-        if (rootNodes.isEmpty()) {
+        if (rootNodes == null) {
             return false;
         }
         while (!rootNodes.isEmpty()) {
             TreeNode node = rootNodes.pop();
-
+            Set<TreeNode> childNodes = new HashSet<>();
+            Stack<TreeNode> loopNodes = new Stack<TreeNode>();
+            loopNodes.push(node);
+            while (!loopNodes.isEmpty()) {
+                TreeNode loopNode = loopNodes.pop();
+                if (loopNode.child == null) {
+                    continue;
+                }
+                for (TreeNode childNode : loopNode.child) {
+                    if (childNodes.contains(childNode)){
+                        return false;
+                    }
+                    childNodes.add(childNode);
+                    loopNodes.push(childNode);
+                }
+            }
         }
+        return true;
     }
 }
