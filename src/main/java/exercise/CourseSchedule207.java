333e324c9d326e8acc2692886ebec77e840e6481
liz <65708133+wlqqlz@users.noreply.github.com>
2024-11-11 14:49:06 +0000

    207-difficult!

--- src/main/java/exercise/CourseSchedule207.java
+++ src/main/java/exercise/CourseSchedule207.java
@@ -1,5 +1,7 @@
 package exercise;
 
+import java.util.Stack;
+
 /**
  * @Author : Ze Li
  * @Date : 11/11/2024 13:24
@@ -7,4 +9,36 @@ package exercise;
  * @Description :
  */
 public class CourseSchedule207 {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        class TreeNode {
+//            many parents, many children
+            TreeNode parent;
+            TreeNode child;
+
+            public TreeNode() {
+            }
+        }
+        TreeNode[] nodes = new TreeNode[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            nodes[i] = new TreeNode();
+        }
+        for (int[] prerequisite : prerequisites) {
+            nodes[prerequisite[0]].parent = nodes[prerequisite[1]];
+            nodes[prerequisite[1]].child = nodes[prerequisite[0]];
+        }
+        Stack<TreeNode> rootNodes = null;
+        for (int i = 0; i < numCourses; i++) {
+            rootNodes = new Stack<>();
+            if (nodes[i].parent == null) {
+                rootNodes.push(nodes[i]);
+            }
+        }
+        if (rootNodes.isEmpty()) {
+            return false;
+        }
+        while (!rootNodes.isEmpty()) {
+            TreeNode node = rootNodes.pop();
+
+        }
+    }
 }
