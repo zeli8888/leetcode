2550d0b477bcf8ee79b7fa24decf93a1c92797ac
Edwin <lize201@mails.ucas.ac.cn>
2025-01-04 01:42:28 +0000

    283 155 done

--- src/main/java/exercise/MoveZeroes283.java
+++ src/main/java/exercise/MoveZeroes283.java
@@ -7,4 +7,17 @@ package exercise;
  * @Description :
  */
 public class MoveZeroes283 {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] != 0){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        while (j < nums.length){
+            nums[j] = 0;
+            j++;
+        }
+    }
 }
