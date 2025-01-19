d6df33de76259e0427d272d255de178f315882ed
Edwin <lize201@mails.ucas.ac.cn>
2025-01-19 16:31:58 +0000

    417

---
+++ src/main/java/exercise/PartitionEqualSubsetSum416.java
@@ -0,0 +1,39 @@
+package exercise;
+
+import java.util.Arrays;
+
+/**
+ * @Author : Ze Li
+ * @Date : 19/01/2025 16:01
+ * @Version : V1.0
+ * @Description :
+ */
+public class PartitionEqualSubsetSum416 {
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        int n = nums.length;
+
+        for(int i : nums) sum+=i;
+
+        if(sum%2!=0) return false;
+
+        sum /= 2;
+
+        boolean[] dp = new boolean[sum+1];
+
+        dp[0] = true;
+
+        for (int j : nums) {
+            for (int i = sum; i > 0; i--) {
+                if (i >= j) {
+                    dp[i] = dp[i] || dp[i-j];
+                }
+            }
+        }
+
+        return dp[sum];
+
+
+    }
+
+}
