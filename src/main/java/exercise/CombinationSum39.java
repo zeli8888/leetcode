8b3ea4185a079ae0509558266eddab3d27804561
liz <65708133+wlqqlz@users.noreply.github.com>
2025-01-29 10:50:34 +0000

    39 not yet

---
+++ src/main/java/exercise/CombinationSum39.java
@@ -0,0 +1,20 @@
+package exercise;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @Author : Ze Li
+ * @Date : 29/01/2025 10:16
+ * @Version : V1.0
+ * @Description :
+ */
+public class CombinationSum39 {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        ArrayList<List<Integer>> ans = new ArrayList<>();
+        for (int candidate : candidates){
+
+        }
+    }
+}
