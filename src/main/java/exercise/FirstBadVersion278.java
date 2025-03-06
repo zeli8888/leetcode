b67a0fc09729d566f70e15c29b573cbc4325ffef
liz <65708133+wlqqlz@users.noreply.github.com>
2025-03-06 13:54:05 +0000

    feat(278): done

---
+++ src/main/java/exercise/FirstBadVersion278.java
@@ -0,0 +1,26 @@
+package exercise;
+
+/**
+ * @Author : Ze Li
+ * @Date : 06/03/2025 12:29
+ * @Version : V1.0
+ * @Description :
+ */
+
+public class FirstBadVersion278 {
+//    public int firstBadVersion(int n) {
+//        int left = 1;
+//        int ans = 1;
+//        int right = n;
+//        while (left <= right){
+//            int mid=left+(right-left)/2;
+//            if (isBadVersion(mid)){
+//                ans = mid;
+//                right = mid-1;
+//            }else{
+//                left = mid+1;
+//            }
+//        }
+//        return ans;
+//    }
+}
