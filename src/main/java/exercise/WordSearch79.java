54e0e3fd7075bb65dd1d0801a576bde29d8e54fc
liz <65708133+wlqqlz@users.noreply.github.com>
2025-03-31 15:02:50 +0100

    feat(79): should succeed!

---
+++ src/main/java/exercise/WordSearch79.java
@@ -0,0 +1,52 @@
+package exercise;
+
+import java.util.Stack;
+
+/**
+ * @Author : Ze Li
+ * @Date : 31/03/2025 10:05
+ * @Version : V1.0
+ * @Description :
+ */
+public class WordSearch79 {
+    public boolean exist(char[][] board, String word) {
+        int m = board.length;
+        int n = board[0].length;
+
+        boolean[][] visited = new boolean[m][n];
+        boolean result = false;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    result = backtrack(board, word, visited, i, j, 0);
+                    if (result) return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean backtrack(char[][] board, String word, boolean[][] visited, int i, int j, int index) {
+        if (index == word.length()) {
+            return true;
+        }
+
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || board[i][j] != word.charAt(index)) {
+            return false;
+        }
+
+        visited[i][j] = true;
+
+        if (backtrack(board, word, visited, i + 1, j, index + 1) ||
+                backtrack(board, word, visited, i - 1, j, index + 1) ||
+                backtrack(board, word, visited, i, j + 1, index + 1) ||
+                backtrack(board, word, visited, i, j - 1, index + 1)) {
+            return true;
+        }
+
+        visited[i][j] = false;
+        return false;
+    }
+}
