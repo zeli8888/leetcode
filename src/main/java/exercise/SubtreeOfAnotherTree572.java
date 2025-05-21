7995162774dc9b948c841a9195da2374a3735992
Edwin <lize201@mails.ucas.ac.cn>
2025-05-22 00:03:07 +0100

    572 done

--- src/main/java/exercise/SubtreeOfAnotherTree572.java
+++ src/main/java/exercise/SubtreeOfAnotherTree572.java
@@ -10,12 +10,19 @@ import course.TreeNode;
  */
 public class SubtreeOfAnotherTree572 {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-
+        return search(root, subRoot);
     }
 
-    public boolean DFS(TreeNode root, TreeNode subRoot) {
-        if (root.val == subRoot.val) {
-            DFS()
-        }
+    private boolean isSameTree(TreeNode root, TreeNode subRoot) {
+        if (root == null && subRoot == null) return true;
+        if (root == null || subRoot == null) return false;
+        if (root.val != subRoot.val) return false;
+        return isSameTree(root.left, subRoot.left) && isSameTree(root.right, subRoot.right);
+    }
+
+    private boolean search(TreeNode root, TreeNode subRoot) {
+        if (root == null) return false;
+        if (isSameTree(root, subRoot)) return true;
+        return search(root.left, subRoot) || search(root.right, subRoot);
     }
 }
