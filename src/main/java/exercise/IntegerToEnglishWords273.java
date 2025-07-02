df6e5e7630163d9b364235526c9ccdacbe5c927e
liz <65708133+wlqqlz@users.noreply.github.com>
2025-07-02 13:34:40 +0100

    273 to go

---
+++ src/main/java/exercise/IntegerToEnglishWords273.java
@@ -0,0 +1,13 @@
+package exercise;
+
+/**
+ * @Author : Ze Li
+ * @Date : 02/07/2025 12:03
+ * @Version : V1.0
+ * @Description :
+ */
+public class IntegerToEnglishWords273 {
+    public String numberToWords(int num) {
+
+    }
+}
